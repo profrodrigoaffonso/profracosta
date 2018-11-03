bd783e327760c10152eda801f81c6e75817d808b
profrodrigoaffonso <profrodrigoaffonso@gmail.com>
2018-11-03 10:33:28 -0300

    commit

--- src/Controller/TestesController.php
+++ src/Controller/TestesController.php
@@ -10,4 +10,16 @@ class TestesController extends AppController
     {
 
     }
+
+    public function req()
+    {
+
+    	$this->loadModel("Comandos");
+
+    	$comando = $this->Comandos->get(1);
+
+    	echo $comando['comando'];
+    	die;
+
+    }
 }
